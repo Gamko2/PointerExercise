389905cf74b05e8cdbcfb8aa2030d97f024bccbb
Gamko2 <3077-916@online.de>
2017-11-02 13:49:12 +0100

    Update Exercise4.cpp

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -9,7 +9,7 @@ int main() {
 	cout << str[0] <<" Pointer position: " << *pointer << " The letter t, hopefully: " <<pointer[3] << endl;
 	pointer = pointer + 2;
 	cout << "Pointer value after increase: " << *pointer << endl;
-	cout <<"The letter r and g I hope: " << pointer[2] << " " << pointer[5];
+	cout <<"The letter r and g I hope: " << pointer[2] << " " << pointer[5] << endl;;
 
 	return 0;
 }
